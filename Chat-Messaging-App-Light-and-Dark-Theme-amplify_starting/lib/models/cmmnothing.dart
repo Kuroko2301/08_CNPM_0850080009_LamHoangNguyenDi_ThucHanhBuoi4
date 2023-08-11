41254854250b58f7341fe3c0c554420494ce0018
Kuroko2301 <giadinhsuper79@gmail.com>
2023-08-11 22:19:56 +0700

    thuchanh4

---
+++ Chat-Messaging-App-Light-and-Dark-Theme-amplify_starting/lib/models/cmmnothing.dart
@@ -0,0 +1,44 @@
+enum ChatMessageType { text, audio, image, video }
+
+enum MessageStatus { notSent, notView, viewed }
+
+class ChatMessage {
+  final String text;
+  final ChatMessageType messageType;
+  final MessageStatus messageStatus;
+  final bool isSender;
+
+  ChatMessage({
+    this.text = '',
+    required this.messageType,
+    required this.messageStatus,
+    required this.isSender,
+  });
+}
+
+List demeChatMessages = [
+  ChatMessage(
+    text: "Hi Sajol,",
+    messageType: ChatMessageType.text,
+    messageStatus: MessageStatus.viewed,
+    isSender: false,
+  ),
+  ChatMessage(
+    text: "Hello, How are you?",
+    messageType: ChatMessageType.text,
+    messageStatus: MessageStatus.viewed,
+    isSender: true,
+  ),
+  ChatMessage(
+    text: "This looks great man!!",
+    messageType: ChatMessageType.text,
+    messageStatus: MessageStatus.viewed,
+    isSender: false,
+  ),
+  ChatMessage(
+    text: "Glad you like it",
+    messageType: ChatMessageType.text,
+    messageStatus: MessageStatus.notView,
+    isSender: true,
+  ),
+];
